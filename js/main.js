57c7a11f49c46328f3fa9038dec8cfada82d152a
Quantumania23 <mikepeace981@gmail.com>
2023-06-16 15:07:21 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const typed = new Typed('.skillset',{
+    strings: ['Frontend Developer', 'Graphic designer','Videographer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
+
+const typeds = new Typed('.slideshow',{
+    strings: ['Mathematically articulate', 'Globetrotter', 'Coffeholic'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
